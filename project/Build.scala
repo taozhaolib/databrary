e5a3aaa6295a42d5ec662a95354117202c30973b
Dylan Simon <dylan@dylex.net>
2013-05-01 11:52:24 -0400

    update play-slick

--- project/Build.scala
+++ project/Build.scala
@@ -9,14 +9,13 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     jdbc,
-    "com.typesafe" %% "play-slick" % "0.3.1",
+    "com.typesafe.play" %% "play-slick" % "0.3.2",
     "com.typesafe.slick" %% "slick" % "1.0.0",
     "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    scalacOptions ++= Seq("-feature","-unchecked","-deprecation"),
-    resolvers += Resolver.url("github repo for play-slick", url("http://loicdescotte.github.com/releases/"))(Resolver.ivyStylePatterns)
+    scalacOptions ++= Seq("-feature","-unchecked","-deprecation")
   )
 
 }
