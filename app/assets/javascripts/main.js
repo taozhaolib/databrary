d0f0c3a0daaf9680442700f04ffad2a4dfab96c2
mikestopcontinues <mikestopcontinues@gmail.com>
2013-08-13 15:10:54 -0400

    responsive design, static menu for small browser windows.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,17 +5,51 @@ var dbjs = {};
 (function () {
     var isTouch = 'ontouchstart' in document.documentElement;
 
-    dbjs.toggleFold = function(fold, folder, folded) {
-        var folders = $(fold + ' ' + folder),
-            foldeds = $(fold + ' ' + folded);
+    dbjs.toggleFold = function (fold, folder, folded) {
+        var $folders = $(fold + ' ' + folder),
+            $foldeds = $(fold + ' ' + folded);
 
-        folders.click(function(e){
+        $folders.click(function (e) {
             $(this).next(folded).slideToggle();
             $(this).toggleClass('unfolded');
         });
 
-        folders.prepend('<span class="arrow"></span>')
-        foldeds.toggle();
+        $folders.prepend('<span class="arrow"></span>')
+        $foldeds.toggle();
+    }
+
+    dbjs.menuReduce = function (menu, position) {
+        var $menu = $(menu),
+            $position = $(position),
+            $origin = $menu.parent(),
+            $menuHeader = $menu.find('h1'),
+            $menuLink = $('<button class="hide"></button>'),
+            toggle = false;
+
+        function menuReducer() {
+            if (toggle == false && $menu.css('z-index') == 1) {
+                $menu.addClass('reduced').prependTo($position);
+                $menuHeader.remove();
+                $menuLink.removeClass('hide');
+                toggle = true;
+            } else if (toggle == true && $menu.css('z-index') == 'auto') {
+                $menu.removeClass('reduced').prependTo($origin);
+                $menuHeader.prependTo($menu);
+                $menuLink.addClass('hide');
+                toggle = false;
+            }
+        }
+
+        $(window).resize(function (e) {
+            menuReducer();
+        });
+
+        menuReducer();
+
+        $menuLink.prependTo($menu);
+        $menuLink.click(function (e) {
+            $menu.toggleClass('dropped');
+        });
     }
 }());
 
@@ -27,11 +61,7 @@ $(document).ready(function () {
     // registration should only appear on the pages it's need. I've added it to-do.
     dbjs.toggleFold('.question', 'h2', 'div');
 
-
-
-
-
-
+    dbjs.menuReduce('#static_links', '#body_main > article > h1:first-child');
 
 
     // refactor AND switch to modals!!!
@@ -49,16 +79,16 @@ $(document).ready(function () {
     // animation helpers REFACTOR
     var waitForFinalEvent = (function () {
         var timers = {};
-        
+
         return function (callback, ms, uniqueId) {
             if (!uniqueId) {
                 uniqueId = "Don't call this twice without a uniqueId";
             }
-            
+
             if (timers[uniqueId]) {
-                clearTimeout (timers[uniqueId]);
+                clearTimeout(timers[uniqueId]);
             }
-            
+
             timers[uniqueId] = setTimeout(callback, ms);
         };
     })();
