e73c4d8aa24e1899326d7e0538c08219ada8e8c0
Mike Stop Continues <mikestopcontinues@gmail.com>
2013-08-28 13:17:50 -0400

    (fixes corruption from IDEA->network problems); login styling and modals begun; study styling and tabs begun; login header fixed; less organized more.

--- app/views/display.scala
+++ app/views/display.scala
@@ -3,8 +3,11 @@ package views.html
  * The file needs to be sorted to views/script/
  */
 
+import play.api.templates.HtmlFormat._
 import util._
-import models.SitePage
+import models._
+import controllers._
+import scala.Some
 
 object display {
   def page(page : SitePage)(implicit site : Site) = PathCrumb(page).toHtml
@@ -26,6 +29,14 @@ object display {
       val n = a / t
       n + " " + u + (if (n == 1) "" else "s")
     } + (if (d < 0) " ago" else "")
+  }
+
+  def plainText(text: Option[String] = Option("")) = {
+    raw("<p>"+text.getOrElse("").split("\\r?\\n").mkString("</p><p>")+"</p>")
+  }
+
+  def gravatarUrl(email: String = "none", size: Int = 64) = {
+    "http://gravatar.com/avatar/"+md5(email.toLowerCase.replaceAll("\\s+", "")).hash+"?s="+size+"&d=mm"
   }
 
   def apply(x : SitePage, full : Boolean = false)(implicit site : Site) = if (full) path(x) else page(x)
