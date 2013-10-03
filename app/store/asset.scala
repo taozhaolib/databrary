5a4c5fa158db072770230d88cfe18e80788fa5bd
Dylan Simon <dylan@dylex.net>
2013-10-03 14:15:02 -0400

    Improve performance of StreamEnumerator ranges
    For files, further improvement could be made by combining the range
    Enumeratee into the stream reader as well.

--- app/store/asset.scala
+++ app/store/asset.scala
@@ -16,11 +16,15 @@ import models._
 sealed trait StreamEnumerator extends Enumerator[Array[Byte]] {
   parent =>
   val size : Long
-  def range(start : Long = 0, end : Long = size - 1) : StreamEnumerator = new StreamEnumerator {
-    val size = end - start + 1
+  protected class Range(protected val start : Long, protected val end : Long) extends StreamEnumerator {
+    protected val stop = end + 1
+    val size = stop - start
     def apply[A](it : Iteratee[Array[Byte], A]) : Future[Iteratee[Array[Byte], A]] =
-      parent.through(StreamEnumerator.range(start, end+1)).apply[A](it)
+      parent.through(StreamEnumerator.range(start, stop)).apply[A](it)
   }
+  /** Produce a new Enumerator reflecting a segment of the current one.
+    * This may invalidate the current enumerator. */
+  def range(start : Long = 0, end : Long = size - 1) = new Range(start, end)
 }
 object StreamEnumerator {
   type Data = Array[Byte]
@@ -28,12 +32,23 @@ object StreamEnumerator {
     val size = buf.length.toLong
     def apply[A](it : Iteratee[Data, A]) : Future[Iteratee[Data, A]] = 
       Enumerator.apply(buf).apply[A](it)
+    override def range(start : Long = 0, end : Long = size - 1) = new Range(start, end) {
+      override def apply[A](it : Iteratee[Data, A]) : Future[Iteratee[Data, A]] =
+        Enumerator.apply(buf.slice(start.toInt, stop.toInt)).apply[A](it)
+    }
   }
   def fromFile(file : File, chunkSize : Int = 32768) = new StreamEnumerator {
+    parent =>
     val size = file.length
     private[this] val input = new FileInputStream(file)
     def apply[A](it : Iteratee[Data, A]) : Future[Iteratee[Data, A]] = 
       Enumerator.fromStream(input, chunkSize).apply[A](it)
+    override def range(start : Long = 0, end : Long = size - 1) = new Range(start, end) {
+      input.skip(start)
+      override def apply[A](it : Iteratee[Data, A]) : Future[Iteratee[Data, A]] = {
+        parent.through(StreamEnumerator.range(0, stop-start)).apply[A](it)
+      }
+    }
   }
   def fromFileGenerator(name : String, gen : File => Unit) : StreamEnumerator = {
     val t = File.createTempFile(name, null)
