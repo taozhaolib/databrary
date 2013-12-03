f14aa4e0de2723dc518cc740f6d7c29312a51399
Mike Stop Continues <mikestopcontinues@gmail.com>
2013-12-03 18:19:00 -0500

    stupid bug fixed

---
+++ app/assets/javascripts/app/controllers/loginPanelCtrl.js
@@ -0,0 +1,15 @@
+define([
+	'app/modules/dbControllers',
+	'app/services/messageService'
+], function (db) {
+	'use strict';
+
+	db.controller('LoginPanelCtrl', ['$scope', '$http', 'MessageService', function ($scope, $http, messageService) {
+		$scope.method = 1;
+
+		$scope.switchMethod = function (method) {
+			$scope.method = method;
+		};
+	}]);
+});
+ 
