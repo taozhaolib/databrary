c99565d0c9b651b608a151101c7e703395076d05
Mike Stop Continues <mikestopcontinues@gmail.com>
2013-12-04 14:31:58 -0500

    update angular; fix requireJS for play implementation

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -4,66 +4,29 @@ require.config({
 	paths: {
 		// requirejs, third-party
 
-		'domReady': [
-			'vendors/domReady-2.0.1/domReady'
-		],
+		'domReady': 'vendors/domReady-2.0.1/domReady',
 
 		// jquery
 
-		'jquery': [
-			'//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-			'vendors/jquery-1.10.2/jquery.min'
-		],
+		'jquery': 'vendors/jquery-1.10.2/jquery.min',
 
 		// angular
 
-		'angular': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular.min',
-			'vendors/angular-1.2.0/angular.min'
-		],
-		'ngAnimate': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-animate.min',
-			'vendors/angular-1.2.0/angular-animate.min'
-		],
-		'ngCookies': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-cookies.min',
-			'vendors/angular-1.2.0/angular-cookies.min'
-		],
-		'ngLoader': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-loader.min',
-			'vendors/angular-1.2.0/angular-loader.min'
-		],
-		'ngResource': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-resource.min',
-			'vendors/angular-1.2.0/angular-resource.min'
-		],
-		'ngRoute': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-route.min',
-			'vendors/angular-1.2.0/angular-route.min'
-		],
-		'ngSanitize': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-sanitize.min',
-			'vendors/angular-1.2.0/angular-sanitize.min'
-		],
-		'ngTouch': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-touch.min',
-			'vendors/angular-1.2.0/angular-touch.min'
-		],
+		'angular': 'vendors/angular-1.2.3/angular.min',
+		'ngAnimate': 'vendors/angular-1.2.3/angular-animate.min',
+		'ngCookies': 'vendors/angular-1.2.3/angular-cookies.min',
+		'ngLoader': 'vendors/angular-1.2.3/angular-loader.min',
+		'ngResource': 'vendors/angular-1.2.3/angular-resource.min',
+		'ngRoute': 'vendors/angular-1.2.3/angular-route.min',
+		'ngSanitize': 'vendors/angular-1.2.3/angular-sanitize.min',
+		'ngTouch': 'vendors/angular-1.2.3/angular-touch.min',
 
-		'ngMocks': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-mocks',
-			'vendors/angular-1.2.0/angular-mocks'
-		],
-		'ngScenario': [
-			'//ajax.googleapis.com/ajax/libs/angularjs/1.2.0/angular-scenario',
-			'vendors/angular-1.2.0/angular-scenario'
-		],
+		'ngMocks': 'vendors/angular-1.2.3/angular-mocks',
+		'ngScenario': 'vendors/angular-1.2.3/angular-scenario',
 
 		// angular, third-party
 
-		'ngStorage': [
-			'vendors/ngStorage/ngStorage.min'
-		]
+		'ngStorage': 'vendors/ngStorage/ngStorage.min'
 	},
 
 	shim: {
