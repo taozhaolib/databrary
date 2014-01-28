662e7c77fe5033f8ee964140dd69aea8debf57c8
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-01-28 16:35:15 -0500

    browserSort works again; no more asset level too

--- app/assets/javascripts/app/directive/browserSort.js
+++ app/assets/javascripts/app/directive/browserSort.js
@@ -5,13 +5,13 @@ define(['app/config/module'], function (module) {
 		var compile = function ($element, $attrs, transclude) {
 			return function ($scope, $element, $attrs) {
 				$scope.toggleSort = function (sort) {
-					$scope.browser.sort[sort].active = !$scope.browser.sort[sort].active;
+					$scope.browser.options[sort].active = !$scope.browser.options[sort].active;
 				};
 
 				$scope.sortClasses = function (sort) {
 					var classes = [];
 
-					if($scope.browser.sort.hasOwnProperty(sort) && $scope.browser.sort[sort].active)
+					if($scope.browser.options.hasOwnProperty(sort) && $scope.browser.options[sort].active)
 						classes.push('on');
 					else
 						classes.push('off');
@@ -24,21 +24,21 @@ define(['app/config/module'], function (module) {
 				$scope.switchRecordSort = function (sort, maybe) {
 					recordToggleState = undefined;
 
-					var sort_i = $scope.browser.recordSorts.index(sort),
-						maybe_i = $scope.browser.recordSorts.index(maybe);
+					var sort_i = $scope.browser.options.record.categories.index(sort),
+						maybe_i = $scope.browser.options.record.categories.index(maybe);
 
 					if(sort.active != maybe.active) {
 						sort.active = !sort.active;
 						maybe.active = !maybe.active;
 					}
 
-					$scope.browser.recordSorts[sort_i] = $scope.browser.recordSorts.splice(maybe_i, 1, $scope.browser.recordSorts[sort_i])[0];
+					$scope.browser.options.record.categories[sort_i] = $scope.browser.options.record.categories.splice(maybe_i, 1, $scope.browser.options.record.categories[sort_i])[0];
 				};
 
 				$scope.addRecordSort = function () {
 					var go = true;
 
-					angular.forEach($scope.browser.recordSorts, function (recordSort) {
+					angular.forEach($scope.browser.options.record.categories, function (recordSort) {
 						if(go && !recordSort.active) {
 							recordSort.active = true;
 							go = false;
@@ -49,7 +49,7 @@ define(['app/config/module'], function (module) {
 				$scope.canAddRecordSort = function () {
 					var canAdd = false;
 
-					angular.forEach($scope.browser.recordSorts, function (recordSort) {
+					angular.forEach($scope.browser.options.record.categories, function (recordSort) {
 						if(!canAdd && !recordSort.active) {
 							canAdd = true;
 						}
@@ -62,10 +62,10 @@ define(['app/config/module'], function (module) {
 					sort.active = false;
 
 					// move to end
-					var sort_i = $scope.browser.recordSorts.index(sort);
+					var sort_i = $scope.browser.options.record.categories.index(sort);
 
-					$scope.browser.recordSorts.splice(sort_i, 1);
-					$scope.browser.recordSorts.push(sort);
+					$scope.browser.options.record.categories.splice(sort_i, 1);
+					$scope.browser.options.record.categories.push(sort);
 				};
 
 				$scope.canRemoveRecordSort = function () {
