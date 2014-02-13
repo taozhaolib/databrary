f8a165691df1dcd5f74ee87f05a0202f244551cf
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-02-13 13:00:09 -0500

    removed mode directives; added volume edit and access links; better phonyView

--- app/assets/javascripts/app/view/phonyView.js
+++ app/assets/javascripts/app/view/phonyView.js
@@ -1,21 +1,21 @@
 define(['app/config/module'], function (module) {
 	'use strict';
 
-	module.controller('PhonyView', ['$scope', '$routeParams', '$http', '$templateCache', function ($scope, $routeParams, $http, $templateCache) {
+	module.controller('PhonyView', ['$scope', '$location', '$http', '$templateCache', function ($scope, $location, $http, $templateCache) {
 		var start = function () {
 			$scope.phonyTemplateUrl = undefined;
 
-			console.log('PhonyView still catches this page!');
+			console.log('PhonyView still catches this page!', $location.url());
 
 			$http
-				.get('/'+$routeParams.page)
+				.get($location.url())
 				.success(function (data) {
-					$templateCache.put($routeParams.page, $(data).filter('#main').text());
-					$scope.phonyTemplateUrl = $routeParams.page;
-				})
-				.error(function (data, status, headers, config) {
+					console.log(data);
 
-				});
+					$templateCache.put($location.url(), $(data).filter('#main').text());
+					$scope.phonyTemplateUrl = $location.url();
+				})
+				.error(function () {});
 		};
 
 		start();
