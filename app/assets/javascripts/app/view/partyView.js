dfbb39407d449f9f02ee977d2fc999d2e159980f
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-02-14 16:10:50 -0500

    party page browser preparedness

--- app/assets/javascripts/app/view/partyView.js
+++ app/assets/javascripts/app/view/partyView.js
@@ -1,8 +1,11 @@
 define(['app/config/module'], function (module) {
 	'use strict';
 
-	module.controller('PartyView', ['$scope', 'party', 'PanelService', function ($scope, party, panelService) {
+	module.controller('PartyView', ['$scope', 'party', 'volumes', 'PanelService', function ($scope, party, volumes, panelService) {
 		$scope.party = party;
+		$scope.volumes = volumes;
+
+		$scope.browser.initialize('party', volumes);
 
 		$scope.$watch('party', function () {
 			panelService.refreshPanels();
