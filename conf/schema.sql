4d6952fb4c6eb6f7f0f6a11e9062675173048639
Dylan Simon <dylan@dylex.net>
2014-02-16 12:58:16 -0500

    Fix volume_access_check
    Having direct take precedence over inherit was incoherent.  Only actual
    immediate grants take precedence now, followed by max(everything else).

--- conf/schema.sql
+++ conf/schema.sql
@@ -197,22 +197,18 @@ COMMENT ON TABLE "volume_access" IS 'Permissions over volumes assigned to users.
 SELECT audit.CREATE_TABLE ('volume_access');
 
 CREATE FUNCTION "volume_access_check" ("volume" integer, "party" integer) RETURNS permission LANGUAGE sql STABLE AS $$
-	WITH va AS (
+	WITH v AS (
 		SELECT party, access, inherit
 		  FROM volume_access 
 		 WHERE volume = $1
 	)
 	SELECT access FROM (
 		SELECT access 
-		  FROM va
+		  FROM v
 		 WHERE party = $2
 	UNION ALL
-		SELECT MAX(LEAST(va.access, ad.direct))
-		  FROM va JOIN authorize_valid ad ON party = parent 
-		 WHERE child = $2
-	UNION ALL
-		SELECT MAX(LEAST(va.inherit, ai.inherit))
-		  FROM va JOIN authorize_view ai ON party = parent 
+		SELECT MAX(GREATEST(LEAST(v.access, a.direct), LEAST(v.inherit, a.inherit)))
+		  FROM v JOIN authorize_view a ON party = parent
 		 WHERE child = $2
 	) a LIMIT 1
 $$;
