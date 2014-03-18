ad3bc86601e3cc162f9df3c0016a43d3d4ab8142
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-03-18 15:28:57 -0400

    type checking for volume fixed

--- app/assets/javascripts/service/typeService.js
+++ app/assets/javascripts/service/typeService.js
@@ -38,7 +38,7 @@ define(['config/module'], function (module) {
 		};
 
 		typeService.isVolume = function (object) {
-			return angular.isObject(object) && (object.body || object.summary);
+			return angular.isObject(object) && object.hasOwnProperty('body');
 		};
 
 		typeService.isRecord = function (object) {
