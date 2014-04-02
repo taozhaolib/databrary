3a7eab5deeedd04df3d27db6ab0dfc897ffe0de2
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-04-02 09:22:12 -0400

    little style updates

--- app/assets/javascripts/filter/format.js
+++ app/assets/javascripts/filter/format.js
@@ -1,16 +1,23 @@
 define(['config/module'], function (module) {
 	'use strict';
 
-	var ent = { "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&apos;" };
+	var ent = {
+		"&": "&amp;",
+		"<": "&lt;",
+		">": "&gt;",
+		'"': "&quot;",
+		"'": "&apos;"
+	};
 
 	module.filter('format', [function () {
 		return function (input, lineMode, raw) {
-			if(!angular.isString(input))
+			if (!angular.isString(input))
 				return '';
 
 			if (!raw)
-				/* TODO: please is there a better way to do this? */
-				input = input.replace(/[&<>"']/g, function(c) { return ent[c]; });
+				input = input.replace(/[&<>"']/g, function (c) {
+					return ent[c];
+				});
 
 			if (lineMode)
 				return input.replace(/: /g, ': <br>');
