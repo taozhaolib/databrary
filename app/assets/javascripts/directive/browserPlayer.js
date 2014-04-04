8c98e3429d2a24423f1664deddffc5901c81c04d
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-04-04 15:34:51 -0400

    osx firefox users alerted

--- app/assets/javascripts/directive/browserPlayer.js
+++ app/assets/javascripts/directive/browserPlayer.js
@@ -1,7 +1,7 @@
 define(['config/module'], function (module) {
 	'use strict';
 
-	module.directive('browserPlayer', ['browserService', function (browserService) {
+	module.directive('browserPlayer', ['browserService', '$window', function (browserService, $window) {
 		var link = function ($scope, $element) {
 			$scope.browser = $scope.browser || browserService;
 
@@ -20,6 +20,10 @@ define(['config/module'], function (module) {
 					type = mimetype.split('/')[0];
 
 				return type == 'text' ? mimetype[1] : type;
+			};
+
+			$scope.supported = function () {
+				return $window.navigator.userAgent.toLowerCase().indexOf('firefox') == -1 && $window.navigator.platform.toLowerCase().indexOf('mac') == -1;
 			};
 		};
 
--- app/assets/javascripts/panel/excerptsPanel.js
+++ app/assets/javascripts/panel/excerptsPanel.js
@@ -1,7 +1,7 @@
 define(['config/module'], function (module) {
 	'use strict';
 
-	module.controller('ExcerptsPanel', ['$scope', 'browserService', '$location', '$timeout', 'typeService', function ($scope, browser, $location, $timeout, type) {
+	module.controller('ExcerptsPanel', ['$scope', 'browserService', '$location', '$timeout', 'typeService', '$window', function ($scope, browser, $location, $timeout, type, $window) {
 		$scope.bootPanel = function () {
 			if (angular.isArray($scope.volume.excerpts) && $scope.volume.excerpts.length > 0)
 				$scope.current = $scope.volume.excerpts[0] || undefined;
@@ -31,6 +31,10 @@ define(['config/module'], function (module) {
 				cls.push('video');
 
 			return cls;
+		};
+
+		$scope.supported = function () {
+			return $window.navigator.userAgent.toLowerCase().indexOf('firefox') == -1 && $window.navigator.platform.toLowerCase().indexOf('mac') == -1;
 		};
 
 		$scope.jump = function (asset) {
