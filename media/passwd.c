45ffdc7adbacc070c443b3ef4a9b46b6b1348d6a
Dylan Simon <dylan@dylex.net>
2014-04-09 12:15:22 -0400

    Back cracklib requirement down to 2.8
    Disable FascistCheckUser for now

--- media/passwd.c
+++ media/passwd.c
@@ -13,7 +13,11 @@ Java_media_Passwd_00024__1check(
 	const char *user = (*env)->GetStringUTFChars(env, juser, 0);
 	const char *name = jname ? (*env)->GetStringUTFChars(env, jname, 0) : NULL;
 
-	const char *r = FascistCheckUser(passwd, GetDefaultCracklibDict(), user, name);
+#ifdef FascistCheckUser // doesn't actually work, but you get the idea. need 2.9
+	const char *r = FascistCheckUser(passwd, NULL, user, name);
+#else // currently have 2.8 on production
+	const char *r = FascistCheck(passwd, NULL);
+#endif
 	jstring jr = r ? (*env)->NewStringUTF(env, r) : NULL;
 
 	if (name)
