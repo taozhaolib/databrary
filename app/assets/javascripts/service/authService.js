e5420f3372b038d19937b84589858335fe845ce8
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-05-01 11:20:53 -0400

    authService uses modern addError interface

--- app/assets/javascripts/service/authService.js
+++ app/assets/javascripts/service/authService.js
@@ -134,8 +134,7 @@ module.factory('authService', [
 
 				messages.addError({
 					body: constants.message('login.error'),
-					errors: res.errors,
-					status: res.status
+					report: res
 				});
 			});
 		};
@@ -155,13 +154,13 @@ module.factory('authService', [
 					type: 'yellow',
 					countdown: 3000
 				});
-			}, function () {
+			}, function (res) {
 				$location.url('/');
 
 				messages.add({
 					body: constants.message('logout.error'),
-					type: 'red',
-					countdown: 3000
+					errors: res.data,
+					status: res.status
 				});
 			});
 		};
@@ -220,7 +219,7 @@ module.factory('authService', [
 				}, function (res) {
 					messages.addError({
 						body: constants.message('superuser.on.error'),
-						errors: res.errors,
+						errors: res.data,
 						status: res.status
 					});
 				});
