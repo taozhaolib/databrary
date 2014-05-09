cac43f608928751ba39ec6a74d5f5afcd56ad85f
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-05-09 16:11:16 -0400

    idle directive to do something after a user has stopped changing input (only current case)

---
+++ app/assets/javascripts/directive/idle.js
@@ -0,0 +1,26 @@
+module.directive('idle', [
+	'pageService', function (page) {
+		var link = function ($scope, $element, $attrs) {
+			var after = $attrs.idleFor ? parseInt($attrs.idleFor) : 3000;
+			var timeout;
+
+			var fn = function () {
+				$scope.$apply($attrs.idle);
+			};
+
+			$element.bind('input', function () {
+				page.$timeout.cancel(timeout);
+				timeout = page.$timeout(fn, after);
+			});
+
+			$element.on('$destroy', function () {
+				page.$timeout.cancel(timeout);
+			});
+		};
+
+		return {
+			restrict: 'A',
+			link: link
+		}
+	}
+]);
