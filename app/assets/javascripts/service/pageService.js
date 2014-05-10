ef42768d8f9a46c9d1a85071185a76526968b5d6
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-05-10 10:42:36 -0400

    working pageService optimization

--- app/assets/javascripts/service/pageService.js
+++ app/assets/javascripts/service/pageService.js
@@ -1,6 +1,23 @@
 module.factory('pageService', [
 	'$injector', function ($injector) {
-		var dependencies = [
+		var page = {
+			analytics: $injector.get('analyticService'),
+			auth: $injector.get('authService'),
+			constants: $injector.get('constantService'),
+			display: $injector.get('displayService'),
+			events: $injector.get('eventService'),
+			gui: $injector.get('guiService'),
+			messages: $injector.get('messageService'),
+			models: $injector.get('modelService'),
+			panels: $injector.get('panelService'),
+			router: $injector.get('routerService'),
+			tooltips: $injector.get('tooltipService'),
+			types: $injector.get('typeService'),
+		};
+
+		//
+
+		angular.forEach([
 			'$anchorScroll',
 			'$animate',
 			'$cacheFactory',
@@ -14,7 +31,6 @@ module.factory('pageService', [
 			'$location',
 			'$log',
 			'$parse',
-			'$provide',
 			'$q',
 			'$rootScope',
 			'$route',
@@ -27,31 +43,18 @@ module.factory('pageService', [
 
 			'$localStorage',
 			'$sessionStorage',
-
-			'analyticService',
-			'authService',
-			'constantService',
-			'displayService',
-			'eventService',
-			'guiService',
-			'messageService',
-			'modelService',
-			'panelService',
-			'routerService',
-			'tooltipService',
-			'typeService',
-		];
-
-		var page = {};
-
-		angular.forEach(dependencies, function (dependency) {
+		], function (dependency) {
 			page[dependency] = $injector.get(dependency);
 		});
+
+		//
 
 		page.$b = $('body');
 		page.$m = $('main');
 		page.$d = $(page.$document);
 		page.$w = $(page.$window);
+
+		//
 
 		return page;
 	}
