bd4df9d4129a50a1d6c09cfdcf63bb304bd74296
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-05-10 10:20:25 -0400

    supercharge pageService and make it more maintainable.

--- app/assets/javascripts/service/pageService.js
+++ app/assets/javascripts/service/pageService.js
@@ -1,66 +1,57 @@
 module.factory('pageService', [
-	'$rootScope',
-	'messageService',
-	'tooltipService',
-	'constantService',
-	'typeService',
-	'routerService',
-	'eventService',
-	'guiService',
-	'modelService',
-	'authService',
-	'analyticService',
-	'$location',
-	'$filter',
-	'$timeout',
-	function ($rootScope, messages, tooltips, constants, types, router, events, gui, models, auth, analytics, $location, $filter, $timeout) {
-		var page = {
-			messages: messages,
-			tooltips: tooltips,
-			constants: constants,
-			types: types,
-			router: router,
-			gui: gui,
-			events: events,
-			models: models,
-			auth: auth,
-			analytics: analytics,
-			$location: $location,
-			$filter: $filter,
-			$timeout: $timeout,
-		};
+	'$injector', function ($injector) {
+		var dependencies = [
+			'$anchorScroll',
+			'$animate',
+			'$cacheFactory',
+			'$compile',
+			'$document',
+			'$filter',
+			'$http',
+			'$injector',
+			'$interpolate',
+			'$interval',
+			'$location',
+			'$log',
+			'$parse',
+			'$provide',
+			'$q',
+			'$rootScope',
+			'$route',
+			'$routeParams',
+			'$sanitize',
+			'$sce',
+			'$templateCache',
+			'$timeout',
+			'$window',
 
-		//
+			'$localStorage',
+			'$sessionStorage',
 
-		page.title = 'Welcome!';
+			'analyticService',
+			'authService',
+			'constantService',
+			'displayService',
+			'eventService',
+			'guiService',
+			'messageService',
+			'modelService',
+			'panelService',
+			'routerService',
+			'tooltipService',
+			'typeService',
+		];
 
-		//
+		var page = {};
 
-		page.loading = false;
-
-		$rootScope.$on('$routeChangeStart', function () {
-			page.loading = true;
+		angular.forEach(dependencies, function (dependency) {
+			page[dependency] = $injector.get(dependency);
 		});
 
-		$rootScope.$on('$routeChangeSuccess', function () {
-			page.loading = false;
-		});
-
-		//
-
-		page.$window = $(window);
-		page.$document = $(document);
-
-		//
-
-		events.listen($rootScope, 'pageService-updateApp', function () {
-			messages.add({
-				type: 'yellow',
-				body: constants.message('app.update') + ' <a href="" onclick="window.location.reload()">Reload</a>.'
-			});
-		});
-
-		//
+		page.$b = $('body');
+		page.$m = $('main');
+		page.$d = $(page.$document);
+		page.$w = $(page.$window);
 
 		return page;
 	}
