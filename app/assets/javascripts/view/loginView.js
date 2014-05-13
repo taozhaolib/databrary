46d99996c72d43499097a7333077a6c7b9fef5c7
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-05-13 11:51:31 -0400

    flatten login panel

--- app/assets/javascripts/view/loginView.js
+++ app/assets/javascripts/view/loginView.js
@@ -1,5 +1,26 @@
 module.controller('LoginView', [
 	'$scope', 'pageService', function ($scope, page) {
 		page.display.title = page.constants.message('page.title.login');
+
+		//
+
+		$scope.method = 'databrary';
+		$scope.loginData = {};
+
+		//
+
+		$scope.switchMethod = function (method) {
+			$scope.method = method;
+		};
+
+		$scope.showMethodLink = function (method) {
+			return $scope.method != method;
+		};
+
+		//
+
+		$scope.submitForm = function () {
+			page.auth.login($scope.loginData);
+		};
 	}
 ]);
