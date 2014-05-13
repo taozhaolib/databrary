037938667b5091d600c0b6947a65ce372be020ed
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-05-13 12:05:32 -0400

    flatten reset panel; bugs

--- app/assets/javascripts/view/resetView.js
+++ app/assets/javascripts/view/resetView.js
@@ -1,5 +1,19 @@
 module.controller('ResetView', [
 	'$scope', 'pageService', function ($scope, page) {
 		page.display.title = page.constants.message('page.title.reset');
+
+		//
+
+		page.events.listen($scope, 'userPasswordForm-init', function (event, form) {
+			form.resetSuccessFn = function () {
+				page.$location.url(page.router.index());
+			};
+
+			form.saveSuccessFn = function () {
+				page.$location.url(page.router.index());
+			};
+
+			event.stopPropagation();
+		});
 	}
 ]);
