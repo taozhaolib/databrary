8a5356d63fa5cf04e0ed500ba742c866f3fbc265
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-05-23 10:13:10 -0400

    front page styles and resize fix

--- app/assets/javascripts/view/homeView.js
+++ app/assets/javascripts/view/homeView.js
@@ -8,26 +8,24 @@ module.controller('HomeView', [
 
 		var $wrap = $('.wrap').first();
 
-		$scope.$watch(function () {
-			return $wrap.css('width');
-		}, function (val, old) {
+		var resize = function (width) {
 			var cols = 1;
-			val = parseInt(val);
+			width = parseInt(width);
 			$scope.columns = [];
 
-			if (val >= 1200) {
+			if (width >= 1200) {
 				cols = 5;
-			} else if (val >= 960) {
+			} else if (width >= 960) {
 				cols = 4;
-			} else if (val >= 720) {
+			} else if (width >= 720) {
 				cols = 3;
-			} else if (val >= 480) {
+			} else if (width >= 480) {
 				cols = 2;
 			}
 
 			var col, i = 0;
 
-			angular.forEach(parties, function (party, key) {
+			angular.forEach(parties, function (party) {
 				if (!party.id) {
 					return;
 				}
@@ -40,6 +38,22 @@ module.controller('HomeView', [
 
 				$scope.columns[col].push(party);
 			});
+		};
+
+		$scope.$watch(function () {
+			return $wrap.outerWidth();
+		}, function (width) {
+			resize(width);
+		});
+
+		page.$w.on('resize.homeView', function () {
+			$scope.$apply(function () {
+				resize($wrap.outerWidth());
+			});
+		});
+
+		$scope.$on('$destroy', function () {
+			page.$w.off('resize.homeView');
 		});
 	}
 ]);
