0cb7d26b7819ddb6bf4110510a39e014258ba868
Jon Coe <jonmcoe@gmail.com>
2014-05-30 14:09:10 -0400

    cleanup to previous

--- app/assets/javascripts/view/homeView.js
+++ app/assets/javascripts/view/homeView.js
@@ -9,28 +9,20 @@ module.controller('HomeView', [
 		var $wrap = $('.wrap').first();
 
 		var resize = function (width) {
-			var parties_per_row = 1;
 			width = parseInt(width);
 			$scope.rows = [];
 
-			if (width >= 1200) {
-				parties_per_row = 5;
-			} else if (width >= 960) {
-				parties_per_row = 4;
-			} else if (width >= 720) {
-				parties_per_row = 3;
-			} else if (width >= 480) {
-				parties_per_row = 2;
-			}
+			var PIXELS_PER_PARTY = 240;
+			var parties_per_row = 
+				Math.max(1, Math.min(5, Math.floor(width/PIXELS_PER_PARTY)));
 
-			var row = -1, i = 0;
-
+			var i = 0;
 			angular.forEach(parties, function (party) {
 				if (!party.id) {
 					return;
 				}
-				
-				if (i % parties_per_row == 0) row++;
+
+				var row = Math.floor(i/parties_per_row);
 				i++;
 
 				if (!$scope.rows[row]) {
