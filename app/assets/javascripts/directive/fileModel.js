285f232b820621b926d93987d260f4f26b806fdb
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-06-11 13:22:37 -0400

    file-model directive has two-way binding and optional file-model-form to set form $dirty

--- app/assets/javascripts/directive/fileModel.js
+++ app/assets/javascripts/directive/fileModel.js
@@ -4,7 +4,22 @@ module.directive('fileModel', [
 			$element.bind('change', function () {
 				$scope.$apply(function () {
 					page.$parse($attrs.fileModel).assign($scope, $element[0].files);
+
+					if ($attrs.fileModelForm) {
+						page.$parse($attrs.fileModelForm)($scope).$setDirty();
+					}
 				});
+			});
+
+			$scope.$watch($attrs.fileModel, function (val) {
+				if (!val) {
+					$element[0].value = '';
+
+					if ($element[0].value) {
+						$element[0].type = "text";
+						$element[0].type = "file";
+					}
+				}
 			});
 		};
 
