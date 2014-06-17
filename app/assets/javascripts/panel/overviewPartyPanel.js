d30eafbfb7c133320f6f87b4d0611321a14bb0ea
Dylan Simon <dylan@dylex.net>
2014-06-17 15:35:48 -0400

    Strip out broken party edit mode (untested)

--- app/assets/javascripts/panel/overviewPartyPanel.js
+++ app/assets/javascripts/panel/overviewPartyPanel.js
@@ -1,40 +1,4 @@
 module.controller('OverviewPartyPanel', [
 	'$scope', 'pageService', function ($scope, page) {
-		$scope.editMode = false;
-
-		var form;
-
-		$scope.edit = function () {
-			$scope.editMode = true;
-			form = $scope.userEditForm;
-
-			form.data = {
-				name: $scope.party.name,
-				email: $scope.party.email,
-				orcid: $scope.party.orcid,
-				duns: $scope.party.duns,
-				affiliation: $scope.party.affiliation,
-				openid: $scope.party.openid
-			}
-		};
-
-		$scope.save = function () {
-			var party = new page.models.Party(form.data);
-
-			party.$save({
-				id: $scope.party.id
-			}, function (res) {
-				console.log(res);
-
-				$scope.cancel();
-			}, function (res) {
-				console.log(res);
-			});
-		};
-
-		$scope.cancel = function () {
-			$scope.editMode = false;
-			form.data = {};
-		};
 	}
 ]);
