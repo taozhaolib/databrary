c03747b2104bd7fea1b282a6d1eb2260d4dc47c7
Jon Coe <jonmcoe@gmail.com>
2014-06-25 13:32:19 -0400

    sort splash page's party tiles by last name

--- app/assets/javascripts/view/homeView.js
+++ app/assets/javascripts/view/homeView.js
@@ -2,7 +2,17 @@ module.controller('HomeView', [
 	'$scope', 'parties', 'volume', 'pageService', function ($scope, parties, volume, page) {
 		page.display.title = page.constants.message('page.title.home');
 
+		var lastName = function(s) {
+			if(!s) return '';	
+			return s.substr(s.lastIndexOf(' '));
+
+		}
+
+		angular.forEach(parties, function(v,k){
+			v.lastName = lastName(v.name);
+		});
 		$scope.parties = parties;
 		$scope.volume = volume;
+
 	}
 ]);
