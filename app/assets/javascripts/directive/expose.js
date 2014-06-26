7e5d914aa869d5969cfe7c283e3cfc3070e2c1b8
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-06-26 10:58:52 -0400

    expose directive to add $element/$attrs to $scope.

---
+++ app/assets/javascripts/directive/expose.js
@@ -0,0 +1,13 @@
+module.directive('expose', [
+	function () {
+		return {
+			restrict: 'A',
+			link: {
+				pre: function ($scope, $element, $attrs) {
+					$scope.$element = $element;
+					$scope.$attrs = $attrs;
+				}
+			}
+		};
+	}
+]);
