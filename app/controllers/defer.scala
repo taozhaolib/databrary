affcaff0598ae4abee50ce12d8f7aa526171e116
Dylan Simon <dylan@dylex.net>
2014-07-01 23:39:57 -0400

    Add site-specific deferrals to handle exceptions

--- app/controllers/defer.scala
+++ app/controllers/defer.scala
@@ -23,8 +23,8 @@ final case class EmptyRequest(request : RequestHeader) extends Request[Unit] {
 /** An action that is created by pre-processing the RequestHeader alone first (with an empty body) using an ActionBuilder[R] to produce a R for input into the main action body.
   * Basically this allows existing ActionBuilders to be used in cases when body processing should be deferred, and all the necessary content for the builder is in the header already.
   */
-final class DeferredAction[R[_]](builder : ActionBuilder[R], block : R[Unit] => Iteratee[Array[Byte], Result]) extends EssentialAction {
-  private class DeferralResult(val request : R[Unit], header : ResponseHeader = ResponseHeader(0)) extends Result(header, Enumerator()) {
+sealed class DeferredAction[R[_]](builder : ActionBuilder[R], block : R[Unit] => Iteratee[Array[Byte], Result]) extends EssentialAction {
+  protected class DeferralResult(val request : R[Unit], header : ResponseHeader = ResponseHeader(0)) extends Result(header, Enumerator()) {
     override def withHeaders(headers: (String, String)*) : Result =
       new DeferralResult(request, header.copy(headers = header.headers ++ headers))
   }
@@ -39,8 +39,27 @@ final class DeferredAction[R[_]](builder : ActionBuilder[R], block : R[Unit] => 
   }
 }
 
+final class DeferredSiteAction[R[_] <: SiteRequest[_]](builder : ActionBuilder[R], block : R[Unit] => Iteratee[Array[Byte], Result])
+  extends DeferredAction(builder, block) {
+  override def apply(request : RequestHeader) : Iteratee[Array[Byte], Result] = Iteratee.flatten {
+    builder.invokeBlock[Unit](EmptyRequest(request), r => Future.successful(new DeferralResult(r)))
+    .map {
+      case dr : DeferralResult if dr.header.status == 0 =>
+	scala.util.control.Exception.handling(classOf[SiteException]).by(e =>
+	  Iteratee.ignore[Array[Byte]].mapM(_ => e.asInstanceOf[SiteException].result(dr.request)))(
+	  block(dr.request).map(_.withHeaders(dr.header.headers.toSeq : _*)))
+	.recoverM {
+	  case e : SiteException => e.result(dr.request)
+	}
+      case r => Iteratee.ignore[Array[Byte]].map(_ => r)
+    }
+  }
+}
+
 object DeferredAction {
   /** Use builder to pre-process the RequestHeader before passing the result onto block. */
   def apply[R[_]](builder : ActionBuilder[R])(block : R[Unit] => Iteratee[Array[Byte], Result]) : EssentialAction =
     new DeferredAction(builder, block)
+  def site[R[_] <: SiteRequest[_]](builder : ActionBuilder[R])(block : R[Unit] => Iteratee[Array[Byte], Result]) : EssentialAction =
+    new DeferredSiteAction(builder, block)
 }
