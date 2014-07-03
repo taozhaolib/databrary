d7e40b933c346baf1376c74304c6e4f6774b5743
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-07-03 11:05:10 -0400

    no id in analytic requests

--- app/assets/model/analytic.js
+++ app/assets/model/analytic.js
@@ -1,10 +1,6 @@
 module.factory('Analytic', [
 	'resourceFactory', '$route', function (resource, $route) {
-		var analytic = resource('/api/null', {
-			id: function () {
-				return $route.current.params.id || undefined;
-			}
-		}, 'analytic');
+		var analytic = resource('/api/null', {}, 'analytic');
 
 		analytic.send = function () {
 			analytic.$cache.removeAll();
