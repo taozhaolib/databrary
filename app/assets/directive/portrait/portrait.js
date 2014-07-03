66010074151dc2669a3fdb018af7ed049b593bf6
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-07-03 14:33:20 -0400

    comment meta fix

--- app/assets/directive/portrait/portrait.js
+++ app/assets/directive/portrait/portrait.js
@@ -1,17 +1,18 @@
 module.directive('portrait', [
-	function () {
-		var compile = function () {
-			return function ($scope, $element, $attrs) {
-				$element.find('.portrait').addClass($element.attr('class'));
-				$element.attr('class', '');
-			};
-		};
-
+	'pageService', function (page) {
 		return {
 			restrict: 'E',
 			templateUrl: 'portrait.html',
 			replace: true,
-			compile: compile
+			link: {
+				pre: function ($scope, $element, $attrs) {
+					$scope.portraitExtra = page.$parse($attrs.extra)($scope);
+				},
+				post: function ($scope, $element, $attrs) {
+					$element.find('.portrait').addClass($attrs.class);
+					$element.attr('class', '');
+				}
+			}
 		};
 	}
 ]);
