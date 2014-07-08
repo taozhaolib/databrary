3dfd41e58e01f3062f1550bcd23d6e944d78da3e
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-07-08 12:34:54 -0400

    slotView uses player directive and has a header

--- app/assets/view/slotView/slotView.js
+++ app/assets/view/slotView/slotView.js
@@ -1,7 +1,6 @@
 module.controller('SlotView', [
 	'$scope', 'slot', 'pageService', function ($scope, slot, page) {
 		$scope.slot = slot;
-		console.log(slot);
 
 		page.display.title = slot.name;
 		page.display.toolbarLinks = [];
