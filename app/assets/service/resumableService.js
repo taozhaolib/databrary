77ca3f244c4fcca336e5cfd7a13672b18de3526f
Jon Coe <jonmcoe@gmail.com>
2014-07-11 14:21:28 -0400

    merge

--- app/assets/service/resumableService.js
+++ app/assets/service/resumableService.js
@@ -1,11 +1,12 @@
-'use strict';
-
+"use strict";
+/*globals Resumable*/
+>>>>>>> Stashed changes
 module.factory('resumableService', [
 	'$rootScope',
 	'typeService',
 	function ($rootScope, typeService){
 		var resumableS = {};
-		resumableS.hello = 'hello';
+		resumableS.makeResumable = function(conf) {return new Resumable(conf);};
 		return resumableS;
 	}
 ]);
