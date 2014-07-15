94fc1d79f51446aec58fa39d1b6231994eaf04f2
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-07-15 12:30:27 -0400

    #1801 - confirm unsaved volume create changes

--- app/assets/javascripts/view/volumeEditView.js
+++ app/assets/javascripts/view/volumeEditView.js
@@ -108,10 +108,6 @@ module.controller('VolumeEditView', [
 		};
 
 		page.display.navigationFn = function (event, val) {
-			if (!volume || val.indexOf('/volume/' + volume.id + '/edit') > -1) {
-				return;
-			}
-
 			for (var id in forms) {
 				if (forms.hasOwnProperty(id) && forms[id] && forms[id].form && forms[id].form.$dirty) {
 					return false;
