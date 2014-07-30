ba8716ab5e6b5278bfb99ef3765e9eeaf200fe2d
Dylan Simon <dylan@dylex.net>
2014-07-30 17:13:04 -0400

    Basic sideways spreadsheet display

--- app/assets/directive/spreadsheet.js
+++ app/assets/directive/spreadsheet.js
@@ -2,6 +2,8 @@
 
 module.directive('spreadsheet', [
   'pageService', function (page) {
+    var MAXREC = 100; // maximum number of records per category per slot
+    var byNumber = function(a,b) { return a-b; };
     var controller = [
       '$scope', function ($scope) {
 
@@ -25,52 +27,88 @@ module.directive('spreadsheet', [
 	});
 	// slots = slots.map(getSlot);
 
-	var data = {
+	// all arrays indexed over row (column-major)
+	var meta = {
 	  id: [],
 	  segment: [],
 	  date: [],
 	  top: [],
 	  consent: [],
 	};
+	var records = {}; // [category + count/MAXREC][metric] = []
+	var depends = {}; // [recordid] = rows
 
-	var processSlot = function (slot, i) {
-	  data.id[i] = slot.id;
-	  data.segment[i] = slot.segment;
-	  data.date[i] = slot.date;
-	  data.top[i] = slot.top;
-	  data.consent[i] = slot.consent;
+	var populateSlot = function (slot, i) {
+	  // populate meta:
+	  meta.id[i] = slot.id;
+	  meta.segment[i] = slot.segment;
+	  meta.date[i] = slot.date;
+	  meta.top[i] = slot.top;
+	  meta.consent[i] = slot.consent;
+
 	  var counts = {};
 	  angular.forEach(slot.records, function (rec) {
 	    var record = volume.records[rec.id];
-	    var c;
-	    if (!(record.category in counts)) {
-	      if (!(record.category in data))
-		data[record.category] = [[]];
-	      counts[record.category] = 1;
-	      c = 0;
-	    } else {
-	      c = counts[record.category] ++;
-	      if (!(c in data[record.category]))
-		data[record.category][c] = [];
+
+	    // populate records:
+	    if (!(record.category in counts))
+	      counts[record.category] = 0;
+	    else if (counts[record.category] >= MAXREC) {
+	      page.$log.warn("Spreadsheet overflow for record category " + record.category + " on slot " + slot.id);
+	      return;
 	    }
-	    data[record.category][c][i] = record;
+	    var c = record.category + (counts[record.category]++)/MAXREC;
+	    var r;
+	    if (!(c in records)) {
+	      r = records[c] = {/*id: []*/};
+	      if (record.category === page.category.participant.id)
+		r.age = [];
+	    } else
+	      r = records[c];
+
+	    // populate measures:
+	    /*r.id[i] = rec.id;*/
+	    if ('age' in r)
+	      r.age[i] = rec.age;
+	    angular.forEach(record.measures, function (v, m) {
+	      if (!(m in r))
+		r[m] = [];
+	      r[m][i] = v;
+	    });
+
+	    // populate depends:
+	    if (!(record.id in depends))
+	      depends[record.id] = [i];
+	    else
+	      depends[record.id].push(i);
 	  });
 	};
 
-	var process = function () {
-	  angular.forEach(slots, processSlot);
+	var populateCols = function () {
+	  meta.records = Object.keys(records).sort(byNumber).map(function (c) {
+	    var cat = page.constants.data.category[Math.floor(c)];
+	    return {
+	      id: c,
+	      category: cat,
+	      metrics: Object.keys(records[c]).sort(byNumber)
+	    };
+	  });
 	};
 
-	process();
-	$scope.data = data;
+	var populate = function () {
+	  angular.forEach(slots, populateSlot);
+	  populateCols();
+	};
+
+	populate();
+	$scope.meta = meta;
+	$scope.records = records;
       }
     ];
 
     return {
       restrict: 'E',
-      scope: {
-	volume: '=',
-      },
+      scope: true,
       templateUrl: 'spreadsheet.html',
       controller: controller,
     };
