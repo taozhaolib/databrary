4700b099f376ddbffcb30e31886e5eda365e7bf0
Dylan Simon <dylan@dylex.net>
2014-08-04 11:34:42 -0400

    Fix permission and display check on party data panel
    Fixes #2203

--- app/assets/javascripts/panel/dataPanel.js
+++ app/assets/javascripts/panel/dataPanel.js
@@ -1,7 +1,7 @@
 module.controller('DataPanel', [
 	'$scope', 'pageService', function ($scope, page) {
 		$scope.refreshPanel = function () {
-			$scope.enabled = $scope.volumes[0];
+			$scope.enabled = $scope.volumes[0] || page.auth.hasAccess('EDIT', $scope.party);
 		}
 	}
 ]);
