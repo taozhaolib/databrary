5ff396a6d813dce6987e6db1c1068ec6348e5dd5
Dylan Simon <dylan@dylex.net>
2014-08-04 00:57:29 -0400

    Switch to range now in org.databrary.iteratees

--- app/store/stream.scala
+++ app/store/stream.scala
@@ -13,7 +13,7 @@ sealed trait StreamEnumerator extends Enumerator[Array[Byte]] {
     protected val stop = end + 1
     val size = stop - start
     def apply[A](it : Iteratee[Array[Byte], A]) : Future[Iteratee[Array[Byte], A]] =
-      parent.through(StreamEnumerator.range(start, stop)).apply[A](it)
+      parent.through(org.databrary.iteratee.Enumeratee.range(start, stop)).apply[A](it)
   }
   /** Produce a new Enumerator reflecting a segment of the current one.
     * This may invalidate the current enumerator. */
@@ -39,7 +39,7 @@ object StreamEnumerator {
     override def range(start : Long = 0, end : Long = size - 1) = new Range(start, end) {
       input.skip(start)
       override def apply[A](it : Iteratee[Data, A]) : Future[Iteratee[Data, A]] = {
-        parent.through(StreamEnumerator.range(0, stop-start)).apply[A](it)
+        parent.through(org.databrary.iteratee.Enumeratee.range(0, stop-start)).apply[A](it)
       }
     }
   }
@@ -51,21 +51,5 @@ object StreamEnumerator {
     } finally {
       t.delete
     }
-  }
-  /** Composition of Enumeratee.take and Enumeratee.drop with Longs. */
-  def range(start : Long, stop : Long) : Enumeratee[Data, Data] = new Enumeratee.CheckDone[Data, Data] {
-    private def next[A](pos : Long, i : Iteratee[Data, A]) =
-      new Enumeratee.CheckDone[Data, Data] { def continue[A](k: K[Data, A]) = Cont(step(pos)(k)) } &> i
-    def step[A](pos : Long)(k : K[Data, A]) : K[Data, Iteratee[Data, A]] = {
-      case in @ Input.El(a) if pos + a.length <= start => Cont(step(pos+a.length)(k))
-      case in @ Input.El(a) if pos < start && pos + a.length > stop => Done(k(Input.El(a.slice((start-pos).toInt, (stop-pos).toInt))), Input.Empty)
-      case in @ Input.El(a) if pos < start => next(pos+a.length, k(Input.El(a.drop((start-pos).toInt))))
-      case in @ Input.El(a) if pos + a.length <= stop => next(pos+a.length, k(in))
-      case in @ Input.El(a) if pos < stop => Done(k(Input.El(a.take((stop-pos).toInt))), Input.Empty)
-      case Input.Empty if pos < start => Cont(step(pos)(k))
-      case Input.Empty if pos < stop => next(pos, k(Input.Empty))
-      case in => Done(Cont(k), in)
-    }
-    def continue[A](k: K[Data, A]) = Cont(step(0)(k))
   }
 }
