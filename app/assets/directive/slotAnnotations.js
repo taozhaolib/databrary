3d6ffd2fc153e49244a62acb08e19ba61e178540
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-08-20 18:47:22 -0400

    timeline - styling timeline records, untested

--- app/assets/directive/slotAnnotations.js
+++ app/assets/directive/slotAnnotations.js
@@ -9,10 +9,29 @@ module.directive('slotAnnotations', [
 				var notes = this;
 				notes.list = [];
 
-				var drawNotes = function (mode) {
-					var $list = $element.find('slot-note-list');
-					var $head = $element.find('slot-note-head');
+				var $list, $head;
 
+				notes.noteStyle = function (note) {
+					var left = note.record.segment[0] / ctrl.clock.duration;
+					var right = ((ctrl.clock.duration - note.record.segment[1]) / ctrl.clock.duration);
+
+					return {
+						'z-index': 500 - note.level,
+						'top': 24 * note.level,
+						'left': left ? left * 100 : 0,
+						'right': right ? right * 100 : 0,
+					};
+				};
+
+				var drawNote = function (levels, level, record) {
+					levels[level].push(record);
+					notes.list.push({
+						record: record,
+						level: level,
+					});
+				};
+
+				var drawNotes = function (mode) {
 					switch (mode) {
 						case 'comments':
 							// todo: comments
@@ -37,39 +56,41 @@ module.directive('slotAnnotations', [
 									return;
 								}
 
-								console.log(record.segment);
-
 								// look for the first level that has enough space
-								for (var i = 0, l = levels.length; i <= l; i++) {
-									if (i < l) { // try each existing level
-										var m = levels[i].length;
+								for (var i = 0, l = levels.length; i < l; i++) { // try existing levels
+									var m = levels[i].length;
 
-										// skip the loop if this level has a full-width record
-										if (m === 1 && !levels[i][0].segment) {
-											continue;
-										}
-
-										// if this record conflicts with any of this level, break
-										for (var j = 0; j < m; j++) {
-											// todo: review this logic
-											if ((record.segment[0] && levels[i][j].segment[1] && levels[i][j].segment[1] >= record.segment[0]) || (record.segment[1] && levels[i][j].segment[0] && levels[i][j].segment[0] <= record.segment[1])) {
-												break;
-											}
-										}
-
-										// all good? then push
-										levels[i].push(record);
-										return;
+									// skip the loop if this level has a full-width record
+									if (m === 1 && !levels[i][0].segment) {
+										continue;
 									}
 
-									// no current level works? add a new one.
-									levels.push([record]);
+									// if this record conflicts with any of this level, break
+									for (var j = 0; j < m; j++) {
+										// todo: review this logic
+										if ((record.segment[0] && levels[i][j].segment[1] && levels[i][j].segment[1] >= record.segment[0]) || (record.segment[1] && levels[i][j].segment[0] && levels[i][j].segment[0] <= record.segment[1])) {
+											break;
+										}
+									}
+
+									// all good? then push
+									drawNote(levels, i, record);
+									return;
 								}
+
+								// no current level works? add a new one.
+								levels.push([]);
+								drawNote(levels, i, record);
 							});
 					}
 				};
 
-				$scope.$watch('ctrl.noteMode', drawNotes);
+				page.$timeout(function () {
+					$list = $element.find('slot-note-list');
+					$head = $element.find('slot-note-head');
+					$scope.$watch('ctrl.noteMode', drawNotes);
+					drawNotes(ctrl.noteMode);
+				});
 
 				return notes;
 			}];
