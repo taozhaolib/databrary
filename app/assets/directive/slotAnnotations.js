1bc404e49583f90b7545108659d78206c400b2a5
Mike Stop Continues <mikestopcontinues@gmail.com>
2014-08-22 18:06:20 -0400

    slot records display as spec'd

--- app/assets/directive/slotAnnotations.js
+++ app/assets/directive/slotAnnotations.js
@@ -1,118 +1,121 @@
 'use strict';
 
 module.directive('slotAnnotations', [
-  'pageService', function (page) {
-    var controller = [
-      '$scope', '$element', '$attrs',
-      function ($scope, $element, $attrs) {
-	var ctrl = page.$parse($attrs.ctrl)($scope);
-	var notes = this;
-	notes.list = [];
+	'pageService', function (page) {
+		var controller = [
+			'$scope', '$element', '$attrs',
+			function ($scope, $element, $attrs) {
+				var ctrl = page.$parse($attrs.ctrl)($scope);
+				var notes = this;
+				notes.list = [];
 
-	var $list, $head;
+				var $list, $head;
 
-	notes.noteStyle = function (note) {
-          if (angular.isArray(note.record.segment)) {
+				notes.noteStyle = function (note) {
+					var left, right;
 
-          }
+					if (angular.isArray(note.record.segment)) {
+						left = note.record.segment && note.record.segment[0] ? (note.record.segment[0] - ctrl.clock.start) / (ctrl.clock.duration - ctrl.clock.start) : 0;
+						right = note.record.segment && note.record.segment[1] ? (ctrl.clock.duration - note.record.segment[1]) / (ctrl.clock.duration - ctrl.clock.start) : 0;
+					} else if (angular.isNumber(note.record.segment)) {
+						left = (note.record.segment - ctrl.clock.start) / (ctrl.clock.duration - ctrl.clock.start);
+						right = 1 - left;
+					}
 
-	  var left = note.record.segment && note.record.segment[0] ? note.record.segment[0] / ctrl.clock.duration : 0;
-	  var right = note.record.segment && note.record.segment[1] ? ((ctrl.clock.duration - note.record.segment[1]) / ctrl.clock.duration) : 0;
+					return {
+						'z-index': note.level,
+						'top': 24 * (note.level + 1),
+						'left': (angular.isNumber(left) ? left * 100 : 0) + '%',
+						'right': (angular.isNumber(right) ? right * 100 : 0) + '%',
+					};
+				};
 
-	  return {
-	    'z-index': note.level,
-	    'top': 24 * (note.level + 1),
-	    'left': left ? left * 100 : 0,
-	    'right': right ? right * 100 : 0,
-	  };
-	};
+				var drawNote = function (levels, level, record) {
+					levels[level].push(record);
+					notes.list.push({
+						record: record,
+						level: level,
+					});
+				};
 
-	var drawNote = function (levels, level, record) {
-	  levels[level].push(record);
-	  notes.list.push({
-	    record: record,
-	    level: level,
-	  });
-	};
+				var drawNotes = function (mode) {
+					notes.list = [];
+					var levels = [];
 
-	var drawNotes = function (mode) {
-	  notes.list = [];
-	  var levels = [];
+					switch (mode) {
+						case 'comments':
+							// todo: comments
+							$head.height(24);
+							break;
 
-	  switch (mode) {
-	    case 'comments':
-	      // todo: comments
-	      $head.height(24);
-	      break;
+						default:
+							if (!ctrl.records[mode]) {
+								ctrl.noteMode = null;
+								$head.height(0);
+								return;
+							}
 
-	    default:
-	      if (!ctrl.records[mode]) {
-		ctrl.noteMode = null;
-		$head.height(0);
-		return;
-	      }
+							ctrl.mode = page.constants.data.category[mode].name;
 
-	      ctrl.mode = page.constants.data.category[mode].name;
+							angular.forEach(ctrl.records[mode], function (record) {
+								var l = levels.length;
 
-	      angular.forEach(ctrl.records[mode], function (record) {
-		var l = levels.length;
+								if (!record.segment) {
+									levels.push([]);
+									drawNote(levels, l, record);
+									return;
+								}
 
-		if (!record.segment) {
-		  levels.push([]);
-		  drawNote(levels, l, record);
-		  return;
-		}
+								// look for the first level that has enough space
+								for (var i = 0; i < l; i++) { // try existing levels
+									var m = levels[i].length;
 
-		// look for the first level that has enough space
-		for (var i = 0; i < l; i++) { // try existing levels
-		  var m = levels[i].length;
+									// skip the loop if this level has a full-width record
+									if (m === 1 && !levels[i][0].segment) {
+										continue;
+									}
 
-		  // skip the loop if this level has a full-width record
-		  if (m === 1 && !levels[i][0].segment) {
-		    continue;
-		  }
+									// if this record conflicts with any of this level, break
+									for (var j = 0; j < m; j++) {
+										// todo: review this logic
+										if ((record.segment[0] && levels[i][j].segment[1] && levels[i][j].segment[1] >= record.segment[0]) || (record.segment[1] && levels[i][j].segment[0] && levels[i][j].segment[0] <= record.segment[1])) {
+											break;
+										}
+									}
 
-		  // if this record conflicts with any of this level, break
-		  for (var j = 0; j < m; j++) {
-		    // todo: review this logic
-		    if ((record.segment[0] && levels[i][j].segment[1] && levels[i][j].segment[1] >= record.segment[0]) || (record.segment[1] && levels[i][j].segment[0] && levels[i][j].segment[0] <= record.segment[1])) {
-		      break;
-		    }
-		  }
+									// all good? then push
+									drawNote(levels, i, record);
+									return;
+								}
 
-		  // all good? then push
-		  drawNote(levels, i, record);
-		  return;
-		}
+								// no current level works? add a new one.
+								levels.push([]);
+								drawNote(levels, i, record);
+							});
 
-		// no current level works? add a new one.
-		levels.push([]);
-		drawNote(levels, i, record);
-	      });
+							$head.height(24 * (levels.length + 1));
+					}
+				};
 
-	      $head.height(24 * (levels.length + 1));
-	  }
-	};
+				page.$timeout(function () {
+					$list = $element.find('.slot-note-list');
+					$head = $element.find('.slot-note-head');
+					$scope.$watch('ctrl.noteMode', drawNotes);
+					drawNotes(ctrl.noteMode);
+				});
 
-	page.$timeout(function () {
-	  $list = $element.find('.slot-note-list');
-	  $head = $element.find('.slot-note-head');
-	  $scope.$watch('ctrl.noteMode', drawNotes);
-	  drawNotes(ctrl.noteMode);
-	});
+				return notes;
+			}
+		];
 
-	return notes;
-      }
-    ];
+		//
 
-    //
-
-    return {
-      restrict: 'E',
-      scope: false,
-      templateUrl: 'slotAnnotations.html',
-      controller: controller,
-      controllerAs: 'notes',
-    };
-  }
+		return {
+			restrict: 'E',
+			scope: false,
+			templateUrl: 'slotAnnotations.html',
+			controller: controller,
+			controllerAs: 'notes',
+		};
+	}
 ]);
