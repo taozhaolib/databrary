97a06ba5e2481cb5a33c8efe8824b64ccb466a9b
drew gordon <drew.s.gordon@gmail.com>
2014-12-18 16:54:40 -0500

    add "the basic idea"

--- app/store/csv.scala
+++ app/store/csv.scala
@@ -7,46 +7,60 @@ import macros.async._
 import site._
 import models._
 
-private class CSVParse {
-  def parseListofList[T](input: Seq[T]) = input match{
-    case x :: xs => input.head.toString 
-    
-  }
-
-  def csvColumnize(l: Seq[Record]): List[(Int, Int)] = {
-    /** get the column headers  by taking all the records and giving back only set of metrics and categories used in volume*/
-    l.map(rec => (rec.measures.list.map(_.metric._id).toList.sortWith(_ < _), rec.category match{
-        case Some(thing) => thing._id
-      }))
-      .distinct
-      .filter(x => !(x._1.isEmpty))
-      .sortBy(t => t._2)
-      .map(f => f._1.map(d => (d, f._2)))
-      .flatten
-      .toList
-  }
-}
-
-
-
 object CSV{
 
   implicit val executionContext = context.foreground
 
 
   def volume(vol: Volume): Future[String] = {
-    val cparse = new CSVParse
+    val cc = new CSVCreate
     vol.records.flatMap{r => 
       vol.containers.flatMap{
         c => c.mapAsync(_.records).map{cont => 
-          /**cparse.parseListofList(c.zip(cont))*/
-          cparse.csvColumnize(r).toString
+          val header = cc.makeHeader(r)
+          
+          cc.makeRow(r, header).toString
+
         }
       }
     }
+  }
+}
 
+
+private class CSVCreate {
+
+
+  def makeHeader(l: Seq[Record]): List[(Option[RecordCategory], Metric[_])] = {
+    /** get the column headers  by taking all the records and giving back only set of metrics and categories used in volume*/
+    l.map(rec => (rec.category, rec.measures.list.map(_.metric).toList.sortWith(_._id < _._id)))
+       .distinct
+       .filter(x => !(x._2.isEmpty))
+       .sortBy(_._1 match{ case Some(thing) => thing._id})
+       .map(f => f._2.map(d => (f._1, d)))
+       .flatten
+       .toList
+  }
+
+  def makeRow(rs: Seq[Record], hs: List[(Option[RecordCategory], Metric[_])]): Seq[Option[Measure[_]]] = { 
+    /** make a row by adding cells to a list */
+    hs.map(h => makeCell(rs, h._1, h._2))
   }
 
 
+
+
+  def makeCell(rs: Seq[Record], ocat: Option[RecordCategory], met: Metric[_]): Option[Measure[_]] = {
+    /** make a cell by locating measures with category and metric */
+    rs.find(r => r.category == ocat)
+        .flatMap(r => r.measures.value(met))
+
+  }
+
+    
 }
+
+
+
+
 
