87e4c1c27eba8bd7ceaced7d84d73e1411c07425
Dylan Simon <dylan@dylex.net>
2015-02-02 13:00:02 -0500

    Return 204 rather than 200 to chunk upload

--- app/controllers/asset.scala
+++ app/controllers/asset.scala
@@ -375,7 +375,7 @@ object AssetApi extends AssetController with ApiController {
         Iteratee.foreach[Array[Byte]](f.write(_))
         .map[Result] { _ =>
           f.close
-          Ok
+          NoContent
         }
         .recover[Result] { case e : Throwable =>
           f.close
