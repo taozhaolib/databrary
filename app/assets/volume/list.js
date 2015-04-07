15f1a1647061f53e2c2edd7977097d9e3d89cd10
Gladys Chan <gladiolus.c@gmail.com>
2015-04-07 18:14:08 -0400

    adding info to profile, wip. not all working yet

--- app/assets/volume/list.js
+++ app/assets/volume/list.js
@@ -10,8 +10,14 @@ app.directive('volumeList', [
     templateUrl: 'volume/list.html',
     link: function ($scope) {
       $scope.page = page;
-
+      $scope.profile = page.$location.path() === '/profile';
+      $scope.shared = function (volume) {
+        return volume.access.some(function (a) {
+          return a.children && a.party.id <= 0; 
+        });
+      };
       $scope.name = function (volume) {
+        console.log(volume);
         return page.$location.path() === '/profile' && volume.alias || volume.name;
       };
     }
