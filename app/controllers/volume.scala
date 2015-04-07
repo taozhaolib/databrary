8abb8aaa6f6ec91571f8c0da7bda0da93e45f82c
Dylan Simon <dylan@dylex.net>
2015-04-07 17:15:44 -0400

    Return all access on volume search
    Bleh, why not, not too much more

--- app/controllers/volume.scala
+++ app/controllers/volume.scala
@@ -296,7 +296,7 @@ object VolumeApi extends VolumeController with ApiController {
     request.obj.json(request.apiOptions).map(Ok(_))
   }
 
-  private final val queryOpts : JsonOptions.Options = Map("access" -> Seq("ADMIN"), "citation" -> Nil)
+  private final val queryOpts : JsonOptions.Options = Map("access" -> Nil, "citation" -> Nil)
   def search = SiteAction.async { implicit request =>
     for {
       vl <- searchResults._2
