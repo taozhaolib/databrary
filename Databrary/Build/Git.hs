8a79d7ab36f403402ac487c0cb6d1579e592e36c
Dylan Simon <dylan@dylex.net>
2015-05-17 14:35:13 -0400

    Move cabal git stuff into Databrary/Build

---
+++ Databrary/Build/Git.hs
@@ -0,0 +1,27 @@
+module Databrary.Build.Git
+  ( setGitVersion
+  ) where
+
+import Control.Applicative ((<$>))
+import Data.Version (parseVersion)
+import Distribution.Simple
+import Distribution.PackageDescription
+import System.IO.Error (catchIOError)
+import System.Process (readProcess)
+import qualified Text.ParserCombinators.ReadP as ReadP
+import Text.Read (readMaybe)
+
+gitDescribe :: IO (Maybe Version)
+gitDescribe = do
+  s <- readProcess "git" ["describe", "--long", "--dirty", "--match", "v[0-9]*"] ""
+    `catchIOError` \_ -> return ""
+  return $ case [ v | (v, "\n") <- ReadP.readP_to_S (ReadP.char 'v' >> parseVersion) s ] of
+    [v@(Version n (c:t))] -> Just $ maybe v (\m -> Version (n ++ replicate (2 - length n) 0 ++ [m]) t) $ readMaybe c
+    _ -> Nothing
+
+setGitVersion :: GenericPackageDescription -> IO GenericPackageDescription
+setGitVersion g = maybe g s <$> gitDescribe where
+  s v = g{ packageDescription = d{ package = p{ pkgVersion = v } } }
+  d = packageDescription g
+  p = package d
+
--- Setup.hs
+++ Setup.hs
@@ -1,30 +1,10 @@
 import Distribution.Simple
-import Distribution.PackageDescription
 
-import Control.Applicative ((<$>))
-import Data.Version (parseVersion)
-import System.IO.Error (catchIOError)
-import System.Process (readProcess)
-import qualified Text.ParserCombinators.ReadP as ReadP
-import Text.Read (readMaybe)
-
-gitDescribe :: IO (Maybe Version)
-gitDescribe = do
-  s <- readProcess "git" ["describe", "--long", "--dirty", "--match", "v[0-9]*"] ""
-    `catchIOError` \_ -> return ""
-  return $ case [ v | (v, "\n") <- ReadP.readP_to_S (ReadP.char 'v' >> parseVersion) s ] of
-    [v@(Version n (c:t))] -> Just $ maybe v (\m -> Version (n ++ replicate (2 - length n) 0 ++ [m]) t) $ readMaybe c
-    _ -> Nothing
-
-setDescVersion :: GenericPackageDescription -> IO GenericPackageDescription
-setDescVersion g = maybe g s <$> gitDescribe where
-  s v = g{ packageDescription = d{ package = p{ pkgVersion = v } } }
-  d = packageDescription g
-  p = package d
+import Databrary.Build.Git
 
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
   { confHook = \(d, i) f -> do
-    d' <- setDescVersion d
+    d' <- setGitVersion d
     confHook simpleUserHooks (d', i) f
   }
