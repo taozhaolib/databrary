21b89d4d6be2659e8a256b3d4dbf7d267a7a52db
Dylan Simon <dylan@dylex.net>
2015-05-20 00:33:54 -0400

    Fix last: node bindir newline

--- Databrary/Setup/Node.hs
+++ Databrary/Setup/Node.hs
@@ -5,7 +5,8 @@ module Databrary.Setup.Node
   , nodeModuleGenerate
   ) where
 
-import Data.List (intercalate)
+import Data.Char (isSpace)
+import Data.List (intercalate, dropWhileEnd)
 import qualified Distribution.ModuleName as Mod
 import Distribution.PackageDescription (PackageDescription)
 import Distribution.Simple.BuildPaths (autogenModulesDir, autogenModuleName)
@@ -40,12 +41,12 @@ nodeInstall verb info = rawSystemProgramConf verb npmProgram (withPrograms info)
 
 nodeModuleGenerate :: Verbosity -> PackageDescription -> LocalBuildInfo -> IO ()
 nodeModuleGenerate verb desc info = do
-  bin <- rawSystemProgramStdoutConf verb npmProgram (withPrograms info) ["bin"]
+  bin <- dropWhileEnd isSpace `fmap` rawSystemProgramStdoutConf verb npmProgram (withPrograms info) ["bin"]
   createDirectoryIfMissingVerbose verb True $ takeDirectory file
   rewriteFile file
-    $ "module " ++ display mod ++ "(binDir) where\n\
+    $ "module " ++ display modn ++ "(binDir) where\n\
     \binDir :: FilePath\n\
     \binDir = " ++ show bin ++ "\n"
   where
-  file = autogenModulesDir info </> Mod.toFilePath mod <.> "hs"
-  mod = Mod.fromString $ intercalate "." $ Mod.components (autogenModuleName desc) ++ ["Node"]
+  file = autogenModulesDir info </> Mod.toFilePath modn <.> "hs"
+  modn = Mod.fromString $ intercalate "." $ Mod.components (autogenModuleName desc) ++ ["Node"]
