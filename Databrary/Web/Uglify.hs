40f75cbb7215e02115111da6fed7f0286dbce939
Dylan Simon <dylan@dylex.net>
2015-05-22 22:36:44 -0400

    Need to uglify app.js first, and omit debug.js

--- Databrary/Web/Uglify.hs
+++ Databrary/Web/Uglify.hs
@@ -4,7 +4,7 @@ module Databrary.Web.Uglify
   ) where
 
 import Control.Applicative ((<$>))
-import Control.Monad (guard)
+import Control.Monad (guard, liftM2)
 import Control.Monad.Trans.Class (lift)
 import Data.List (isPrefixOf)
 import qualified System.FilePath as FP
@@ -15,10 +15,10 @@ import Databrary.Web.Files
 
 generateUglifyJS :: WebGenerator
 generateUglifyJS f t = do
-  jl <- lift $ filter (not . isPrefixOf "lib/" . webFileRel) <$> findWebFiles ".js"
+  jl <- lift $ ("app.js" :) . filter (not . (liftM2 (||) (isPrefixOf "lib/") (`elem` ["app.js", "debug.js"])) . webFileRel) <$> findWebFiles ".js"
   guard (not $ null jl)
   jt <- mapM webFileTime jl
   lift $ webRegenerate (maximum jt) f t $ \wf -> do
     let wfm = wf <.> ".map"
-    callProcess (binDir FP.</> "uglifyjs") $ ["--output", webFileAbs wf, "--source-map", webFileAbs wfm, "--source-map-url", webFileRel wfm, "--prefix", "relative", "--lint", "--screw-ie8", "--mangle", "--compress", "--define", "DEBUG=false"]
+    callProcess (binDir FP.</> "uglifyjs") $ ["--output", webFileAbs wf, "--source-map", webFileAbs wfm, "--source-map-url", webFileRel wfm, "--prefix", "relative", "--screw-ie8", "--mangle", "--compress", "--define", "DEBUG=false"]
       ++ map webFileAbs jl
