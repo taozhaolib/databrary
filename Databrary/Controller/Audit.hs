48bb03610c15b263b6eb32e0bb7ef2f3de699ecb
Dylan Simon <dylan@dylex.net>
2015-06-03 10:56:06 -0400

    Remove duplicates from news feed
    Fixes #3515

--- Databrary/Controller/Audit.hs
+++ Databrary/Controller/Audit.hs
@@ -4,21 +4,24 @@ module Databrary.Controller.Audit
   ) where
 
 import Control.Arrow (second)
+import Data.Function (on)
+import Data.List (nubBy)
 import Data.Ord (comparing)
 
 import Databrary.Ops
 import qualified Databrary.JSON as JSON
-import Databrary.Model.VolumeAccess
-import Databrary.Model.Authorize
 import Databrary.Model.Party
+import Databrary.Model.Authorize
+import Databrary.Model.Volume
+import Databrary.Model.VolumeAccess
 import Databrary.HTTP.Path.Parser
 import Databrary.Action
 import Databrary.Controller.Volume
 
 viewActivity :: AppRoute ()
 viewActivity = action GET (pathJSON >/> "activity") $ \() -> withAuth $ do
-  vl <- mapM (liftK second vol) =<< lookupVolumeActivity 8
-  al <- map (second $ ("party" JSON..=) . partyJSON) <$> lookupAuthorizeActivity 8
+  vl <- mapM (liftK second vol) . nubBy ((==) `on` volumeId . snd) =<< lookupVolumeActivity 8
+  al <- map (second $ ("party" JSON..=) . partyJSON) . nubBy ((==) `on` partyId . snd) <$> lookupAuthorizeActivity 8
   okResponse [] $ JSON.toJSON $ map ent $ take 12 $ mergeBy (comparing fst) vl al
   where
   vol v = ("volume" JSON..=) <$> volumeJSONQuery v [("access", Just "ADMIN")]
