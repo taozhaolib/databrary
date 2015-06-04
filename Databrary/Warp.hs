603de5802e248c7c1a4336e71d54644d4c1de21b
Dylan Simon <dylan@dylex.net>
2015-06-04 16:50:01 -0400

    Add TLS support, tested

--- Databrary/Warp.hs
+++ Databrary/Warp.hs
@@ -1,12 +1,18 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP, OverloadedStrings #-}
 module Main (main) where
 
+#ifdef VERSION_warp_tls
+import Control.Applicative ((<$>), (<*>))
+#endif
 import Control.Exception (evaluate)
 import Control.Monad.Reader (runReaderT)
 import qualified Data.ByteString.Char8 as BSC
 import qualified Data.Configurator as C
 import Data.Version (showVersion)
 import qualified Network.Wai.Handler.Warp as Warp
+#ifdef VERSION_warp_tls
+import qualified Network.Wai.Handler.WarpTLS as WarpTLS
+#endif
 
 import Paths_databrary (version, getDataFileName)
 import Databrary.Service.Types (serviceDB)
@@ -19,11 +25,22 @@ main :: IO ()
 main = do
   conf <- loadConfig
   port <- C.require conf "port"
+#ifdef VERSION_warp_tls
+  cert <- C.lookup conf "ssl.cert"
+  key <- C.lookup conf "ssl.key"
+#endif
   routes <- evaluate routeMap
   withService conf $ \rc -> do
     schema <- getDataFileName "schema"
     runReaderT (updateDBSchema schema) (serviceDB rc)
+#ifdef VERSION_warp_tls
+    maybe
+      Warp.runSettings
+      WarpTLS.runTLS
+      (WarpTLS.tlsSettings <$> cert <*> key)
+#else
     Warp.runSettings
+#endif
       ( Warp.setPort port
       $ Warp.setTimeout 300
       $ Warp.setServerName (BSC.pack $ "databrary/" ++ showVersion version)
