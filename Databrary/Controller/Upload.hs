fc453f26739f855de47147bef6bd48a6a1ea18dd
Dylan Simon <dylan@dylex.net>
2015-06-04 00:10:05 -0400

    Switch upload chunk IO to posix
    To avoid high-level file locking

--- Databrary/Controller/Upload.hs
+++ Databrary/Controller/Upload.hs
@@ -12,15 +12,19 @@ import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (ReaderT)
 import Control.Monad.Trans.Class (lift)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Unsafe as BSU
 import Data.ByteString.Lazy.Internal (defaultChunkSize)
 import Data.Int (Int64)
 import Data.Maybe (isJust)
 import Data.Word (Word64)
+import Foreign.C.Types (CSize(..))
+import Foreign.Marshal.Array (allocaArray, peekArray)
+import Foreign.Ptr (castPtr)
 import Network.HTTP.Types (ok200, noContent204, badRequest400)
 import qualified Network.Wai as Wai
-import System.IO (withBinaryFile, SeekMode(AbsoluteSeek), IOMode(WriteMode, ReadMode), hSeek)
+import System.IO (SeekMode(AbsoluteSeek))
 import System.Posix.Files.ByteString (setFdSize)
-import System.Posix.IO.ByteString (openFd, OpenMode(WriteOnly), defaultFileFlags, exclusive, closeFd)
+import System.Posix.IO.ByteString (openFd, OpenMode(ReadOnly, WriteOnly), defaultFileFlags, exclusive, closeFd, fdSeek, fdWriteBuf, fdReadBuf)
 import System.Posix.Types (COff(..))
 
 import Databrary.Has (view, peeks)
@@ -30,7 +34,6 @@ import Databrary.Model.Permission
 import Databrary.Model.Volume
 import Databrary.Model.Format
 import Databrary.Model.Token
-import Databrary.Files
 import Databrary.Store.Upload
 import Databrary.Store.Asset
 import Databrary.HTTP.Form.Deform
@@ -80,19 +83,24 @@ uploadChunk = action POST (pathJSON </< "upload") $ \() -> withAuth $ do
     Wai.KnownLength l | l /= len -> badLength
     _ -> return ()
   rb <- peeks Wai.requestBody
-  n <- liftIO $ withBinaryFile (toFilePath file) WriteMode $ \h -> do
-    hSeek h AbsoluteSeek (toInteger off)
+  n <- liftIO $ bracket
+    (openFd file WriteOnly Nothing defaultFileFlags)
+    closeFd $ \h -> do
+    _ <- fdSeek h AbsoluteSeek (COff off)
     let block n = do
           b <- rb
           if BS.null b
             then return n
             else do
               let n' = n + fromIntegral (BS.length b)
+                  write b' = do
+                    w <- BSU.unsafeUseAsCStringLen b' $ \(buf, siz) -> fdWriteBuf h (castPtr buf) (fromIntegral siz)
+                    if w < fromIntegral (BS.length b')
+                      then write $ BS.drop (fromIntegral w) b'
+                      else block n'
               if n' > len
                 then return n'
-                else do
-                  BS.hPut h b
-                  block n'
+                else write b
     block 0
   when (n /= len) $ do
     -- TODO: clear block (maybe wait for calloc)
@@ -103,13 +111,21 @@ testChunk :: AppRoute ()
 testChunk = action GET (pathJSON </< "upload") $ \() -> withAuth $ do
   (up, off, len) <- runForm Nothing chunkForm
   file <- peeks $ uploadFile up
-  r <- liftIO $ withBinaryFile (toFilePath file) ReadMode $ \h -> do
-    hSeek h AbsoluteSeek (toInteger off)
-    let block 0 = return False
-        block n = do
-          b <- BS.hGetSome h $ fromIntegral $ n `min` fromIntegral defaultChunkSize
-          if BS.any (0 /=) b
-            then return True
-            else block $ n - fromIntegral (BS.length b)
-    block len
+  r <- liftIO $ bracket
+    (openFd file ReadOnly Nothing defaultFileFlags)
+    closeFd $ \h -> do
+    _ <- fdSeek h AbsoluteSeek (COff off)
+    allocaArray bufsiz $ \buf -> do
+      let block 0 = return False
+          block n = do
+            r <- fdReadBuf h buf $ n `min` fromIntegral bufsiz
+            a <- peekArray (fromIntegral r) buf
+            if r == 0
+              then return False -- really should be error
+              else if any (0 /=) a
+                then return True
+                else block $ n - r
+      block (CSize len)
   emptyResponse (if r then ok200 else noContent204) []
+  where
+  bufsiz = fromIntegral defaultChunkSize
